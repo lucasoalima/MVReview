5a1c66c6e49aca5c4038fce57b9cea0b5a575724
Lucas Lima <lucas_soalima@hotmail.com>
2022-01-14 22:59:11 -0300

    props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,53 +3,56 @@ import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/pagination";
-import {MoviePage } from "types/movie"
+import { MoviePage } from "types/movie";
 
-function Listing(){
-    
-    const[pageNumber, setPageNumber] = useState(0);
+function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
 
-    useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {const data = response.data as MoviePage;
-        setPageNumber(data.number);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
 
-    
+  const movie = {
+    id: 1,
+    image:
+      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
 
-    return(
-        <>
-        <p>{pageNumber}</p>
-        <Pagination/>
+  return (
+    <>
+      <Pagination />
 
-
-    <div className="container">
+      <div className="container">
         <div className="row">
-
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard/>
+          {page.content.map((movie) => (
+            <div key ={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
             </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard/>
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard/>
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard/>
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard/>
-            </div>
-    </div>
-
+          ))}
         </div>
-
-        
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Listing;
