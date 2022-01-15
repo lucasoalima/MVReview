2d21d314e2b7a733a8daf56066b12099d14515be
Lucas Lima <lucas_soalima@hotmail.com>
2022-01-14 21:36:45 -0300

    use effects

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,27 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/pagination";
+import {MoviePage } from "types/movie"
 
 function Listing(){
     
-    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {console.log(response.data);
+    const[pageNumber, setPageNumber] = useState(0);
+
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {const data = response.data as MoviePage;
+        setPageNumber(data.number);
+        });
+    }, []);
+
     
-    });
 
     return(
         <>
+        <p>{pageNumber}</p>
         <Pagination/>
 
 
