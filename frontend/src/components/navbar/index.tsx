1088b01a18f2fd193cbf910fd1ea640343df4e0b
Lucas Lima <lucas_soalima@hotmail.com>
2022-01-14 21:23:55 -0300

    first request

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
         <nav className="container">
             <div className="dsmovie-nav-content">
-                <h1>MVavaliator</h1>
+                <h1>MVAvaliator</h1>
                 <a href="https://github.com/lucasoalima/" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon/>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,14 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/pagination";
 
 function Listing(){
     
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {console.log(response.data);
+    
+    });
+
     return(
         <>
         <Pagination/>
